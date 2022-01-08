e55cad84763fc9e2e42cb029d6a466a2b7b9cfe3
NITO666 <lagun-01@mail.ru>
2022-01-08 06:07:55 +0300

    feat:add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let b = String(n)
+    c = '';
+    for(let i = b.length-1;i >= 0 ;i--){
+        if (b[i] === '-'){
+        c = c + ''
+        }else{
+        c = c + b[i]
+        }
+        
+    }
+    return Number(c)
 }
